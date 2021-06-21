bd5a70e260c526ab30416827690bde352bc9f858
moul <moul@163.com>
2021-06-21 18:19:41 +0800

    更新

--- message/message.go
+++ message/message.go
@@ -5,53 +5,52 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func (x*Message)Put(){
+func (x *Message) Put() {
 	Pool.Put(x)
 }
 
-
-func (x*Message)SetKey(key string){
+func (x *Message) SetKey(key string) {
 	x.Key = key
 
 }
-func (x*Message)SetValue(value []byte){
+func (x *Message) SetValue(value []byte) {
 	x.Value = value
 }
-func (x*Message)SetTopic(topic string){
+func (x *Message) SetTopic(topic string) {
 	x.Topic = topic
 }
-func (x*Message)SetHeaders(headers map[string]string){
+func (x *Message) SetHeaders(headers map[string]string) {
 	x.Headers = headers
 }
-func (x*Message)SetTimesTamp(time int64){
+func (x *Message) SetTimesTamp(time int64) {
 	x.TimesTamp = time
 }
 
-func (x*Message)GetHeader(key string)string{
-	h,ok:=x.Headers[key]
-	if !ok{
+func (x *Message) GetHeader(key string) string {
+	h, ok := x.Headers[key]
+	if !ok {
 		return ""
 	}
 	return h
 }
-func (x*Message)SetHeader(key,value string){
+func (x *Message) SetHeader(key, value string) {
 	x.Headers[key] = value
 }
 
-func (x*Message)ReadProtobuf(ptr proto.Message)error{
+func (x *Message) ReadProtobuf(ptr proto.Message) error {
 	return proto.Unmarshal(x.GetValue(), ptr)
 }
-func (x*Message)ReadJson(ptr interface{})error{
+func (x *Message) ReadJson(ptr interface{}) error {
 	return json.Unmarshal(x.GetValue(), ptr)
 }
-func (x*Message)ReadString()string{
+func (x *Message) ReadString() string {
 	return string(x.GetValue())
 }
-func (x*Message)ReadBytes()[]byte{
+func (x *Message) ReadBytes() []byte {
 	return x.GetValue()
 }
 
-func (x*Message)SetProducer(name string){
+func (x *Message) SetProducer(name string) {
 	x.Producer = name
 
 }
